7c303fda0127b01a1fb117b5ae7c3ca37891496d
Viet Anh <vietanhtr923@gmail.com>
2023-10-14 22:39:44 +0700

    commit

---
+++ src/Lab1/ChoosingOption.java
@@ -0,0 +1,12 @@
+package Lab1;
+import javax.swing.JOptionPane;
+
+public class ChoosingOption {
+    public static void main(String[] args) {
+        int option = JOptionPane.showConfirmDialog(null,
+                "Do you want to change to the first class ticket?");
+        JOptionPane.showMessageDialog(null, "You have choosen: "
+                        + (option == JOptionPane.YES_OPTION?"Yes":"No"));
+        System.exit(0);
+    }
+}
