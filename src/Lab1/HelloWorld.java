91d19a5e7727d5b7382db4fbbe2a3c271d8ac8b5
Viet Anh <vietanhtr923@gmail.com>
2023-10-14 14:38:22 +0700

    fasdf

---
+++ src/Lab1/HelloWorld.java
@@ -0,0 +1,11 @@
+package Lab1;
+//Example 1 : HelloWorld.java
+// Text printing program
+
+public class HelloWorld {
+    public  static void main(String arg[]){
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \t world!");
+
+    }
+}
