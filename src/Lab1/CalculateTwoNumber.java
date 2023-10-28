c62d3130aa96050c44fa139cd41eb63aa9d35d33
Viet Anh <vietanhtr923@gmail.com>
2023-10-28 23:11:13 +0700

    commit

--- src/Lab1/CalculateTwoNumber.java
+++ src/Lab1/CalculateTwoNumber.java
@@ -13,13 +13,14 @@ public class CaculateTwoNumber{
                     "Please input the second number: ", "Input the second number",
                     JOptionPane.INFORMATION_MESSAGE);
             num2 = Double.parseDouble(strNum2);
-            if (num2 == 0) JOptionPane.showMessageDialog(null,"Hay nhap lai so khac 0");}
+            if (num2 == 0)                             // check if num2 = 0 then request to enter again
+                JOptionPane.showMessageDialog(null,"Hay nhap lai so khac 0");}
             while (num2 == 0);
 
-            double sum = num1 + num2;
-            double dif = num1 - num2;
-            double pro = num1 * num2;
-            double quo = num1 / num2;
+            double sum = num1 + num2;                   // caculate sum
+            double dif = num1 - num2;                   // difference
+            double pro = num1 * num2;                   // product
+            double quo = num1 / num2;                   // quotient
 
             System.out.println("Tong cua 2 so: " + sum);
             System.out.println("Hieu cua 2 so: " + dif);
