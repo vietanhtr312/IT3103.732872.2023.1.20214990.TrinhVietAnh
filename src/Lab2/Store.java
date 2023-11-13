95c652899ca03a37cdb0849034663869627dcd05
Viet Anh <vietanhtr923@gmail.com>
2023-11-14 01:40:18 +0700

    store

--- src/Lab2/Store.java
+++ src/Lab2/Store.java
@@ -1,2 +1,59 @@
-package Lab2;public class Store {
+package Lab2;                                               // Trịnh Việt Anh 20214990
+
+public class Store {
+    public static final int MAX_NUMBERS_ITEM = 200;
+    private DigitalVideoDisc itemInStore[] = new DigitalVideoDisc[MAX_NUMBERS_ITEM];
+    private int qtyNumber;
+
+                                                                    // method to add each of disc
+    public void addDVD(DigitalVideoDisc dvd) {
+        boolean check = false;
+        if (qtyNumber == MAX_NUMBERS_ITEM) {                                // check if the store is full
+            System.out.println("The STORE is almost full");
+        } else {
+            for (int i = 0; i < qtyNumber; i++)                             // traverse the list
+                if (itemInStore[i].equals(dvd)) {                           // to check if the disc has been added
+                    System.out.println("The disc has been added");
+                    check = true;
+                    break;
+                }
+            if (!check) {                                                   // If not
+                itemInStore[qtyNumber] = dvd;                               // add disc to the list
+                qtyNumber += 1;                                             // add quantity
+                System.out.println("Store add successful");
+            }
+        }
+    }
+
+                                                                            // method to add a list of disc
+    public void  addDVD(DigitalVideoDisc[] dvdList){
+        if (qtyNumber + dvdList.length > MAX_NUMBERS_ITEM) {                    // check if the store can add all the list or not
+            System.out.println("Can not add all the list to store");
+        }else {
+            for (DigitalVideoDisc digitalVideoDisc : dvdList) {                 // add sequence of disc
+                addDVD(digitalVideoDisc);
+            }
+        }
+    }
+
+                                                                        // method to remove disc
+        public void removeDVD(DigitalVideoDisc dvd){
+        int j = 0;
+        boolean check = false;
+        for (int i = 0; i < qtyNumber; i++) {                         // replace the removed DVD by the next DVD
+            if (!itemInStore[i].equals(dvd)) {                        // in the list
+                itemInStore[j] = itemInStore[i];
+                j++;
+            } else {
+                itemInStore[i] = null;                                 //  delete DVD from the list
+                check = true;
+            }
+        }
+        if (!check) System.out.println("The disc has not been added");  // if no DVD removed
+        else {
+            System.out.println("Store remove successful");
+            qtyNumber -= 1;
+        }
+    }
+
 }
--- src/Lab2/StoreTest.java
+++ src/Lab2/StoreTest.java
@@ -1,2 +1,23 @@
-package Lab2;public class StoreTest {
+package Lab2;                                             // Trịnh Việt Anh 20214990
+
+public class StoreTest {
+    public static void main(String[] args) {
+        //Create a new cart
+        Store aStore = new Store();
+
+        //Create new dvd objects and add them to the cart
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                "Animation", "Rogers Allers", 87, 19.95f);
+        aStore.addDVD(dvd1);                            // test add one disc
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+                "Science Fiction", "George Lucas", 87, 24.95f);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+                "Animation", 18.99f);
+        DigitalVideoDisc list[] = {dvd2, dvd3};         //test add list of disc
+        aStore.addDVD(list);
+
+        aStore.removeDVD(dvd2);                         //test remove disc
+    }
 }
